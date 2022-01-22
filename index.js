63fdce8c8f007de7d4068db3f7e47582707c667c
Username <Email address>
2022-01-22 17:40:36 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://syi04013:as8754@cluster0.7a5ch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB connected...'))
 .catch(error => console.log(error))
 
-app.get('/', (req, res) => res.send('Develog!'))
+app.get('/', (req, res) => res.send('ddsalog!'))
+
+app.post('/register',(req, res) =>{
+    //회원가입에 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다
+    const user = new User(req.body)
+    
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false,err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
